907125a51e59285e124d84b2a366acec2a96752b
danielrkling <104110213+danielrkling@users.noreply.github.com>
2023-08-17 17:08:32 -0400

    1 file modified, 181 deleted

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  // externals: {
-  //   "monaco-editor": "monaco"
-  // },
+  externals: {
+    "monaco-editor": "monaco"
+  },
 };
